ad9d96e803b0a3ab45c843487c6d26cb0eb4c069
ljf <ljf+git@grimaud.me>
2024-10-17 19:00:24 +0200

    [enh] Use babel instead of the basic i18n

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
 # Yunohost landingpage
 
-## How to contribute ?
+## How to contribute
 !! Please do not edit directly index.*.html files
 
-1. `apt install python3-jinja2`
-1. Clone the repo and `cd` in it
-2. Run `bash bin/fetch_assets`
-3. Make your change inside `template.html` view or in `locales/`
-4. (optionnaly) run `bin/localize.py` to see the result
+```
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+bash bin/fetch_assets
+```
 
+Make your change inside `index.html` view
+
+
+## Translations
+
+It's based on Flask-Babel : https://python-babel.github.io/flask-babel/
+```
+source venv/bin/activate
+
+# Extract the english sentences from the code, needed if you modified it
+pybabel extract -F babel.cfg -o messages.pot index.html
+
+# If working on a new locale: initialize it (in this example: fr)
+pybabel init -i messages.pot -d translations -l fr
+# Otherwise, update the existing .po:
+pybabel update -i messages.pot -d translations
+
+# ... translate stuff in translations/<lang>/LC_MESSAGES/messages.po
+# re-run the 'update' command to let Babel properly format the text
+# then compile:
+pybabel compile -d translations
+
+# Generate translated html
+python3 bin/localize.py
+```
 ## How to deploy
 
 ## TODO
--- bin/localize.py
+++ bin/localize.py
@@ -1,29 +1,27 @@
 import yaml
 import os
 from jinja2 import Environment, FileSystemLoader
+from babel.support import Translations
 
-LOCALES_PATH = "locales"
+LOCALES_PATH = "translations"
 TEMPLATES_PATH = "./"
 DIST_PATH = "dist"
 DEFAULT_LOCALE = "en"
 
 # Find supported locales
-locales = [file.replace('.yaml', '') for file in os.listdir(LOCALES_PATH)]
-
-# Load default locale
-with open(f"{LOCALES_PATH}/{DEFAULT_LOCALE}.yaml") as default_locale_file:
-    default_text = yaml.safe_load(default_locale_file)
+locales = [file for file in os.listdir(LOCALES_PATH)]
 
 # Load template
-env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
+env = Environment(
+    extensions=['jinja2.ext.i18n'],
+    loader=FileSystemLoader(TEMPLATES_PATH))
 template = env.get_template("index.html")
 
 # Generate translated html for each locales
 for locale in locales:
-    with open(f"{LOCALES_PATH}/{locale}.yaml") as locale_file:
-        translations = yaml.safe_load(locale_file)
-
-    translated_html = template.render(default_text | translations)
+    translations = Translations.load(LOCALES_PATH, [locale])
+    env.install_gettext_translations(translations)
+    translated_html = template.render({"lang": locale})
 
     with open(f"{DIST_PATH}/index.{locale}.html", "w") as file:
         file.write(translated_html)

5cad30cd8875e00e7d33d7b25863596f84d0bb08
Florian Schmitt <mrflos@gmail.com>
2024-12-19 10:55:30 +0300

    yolo first implementation of donate page + multi-architecture tailwind bin

--- bin/localize.py
+++ bin/localize.py
@@ -19,22 +19,30 @@ TEMPLATES_PATH = LANDINGPAGE_DIR
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("output", type=Path, nargs="?", default=LANDINGPAGE_DIR / "dist", help="Output directory of assets and generated HTMLs")
+    parser.add_argument(
+        "output",
+        type=Path,
+        nargs="?",
+        default=LANDINGPAGE_DIR / "dist",
+        help="Output directory of assets and generated HTMLs",
+    )
     args = parser.parse_args()
 
     def no_p(non_p_string: str) -> str:
-        """ Strip enclosing paragraph marks, <p> ... </p>,
-            which markdown() forces, and which interfere with some jinja2 layout
+        """Strip enclosing paragraph marks, <p> ... </p>,
+        which markdown() forces, and which interfere with some jinja2 layout
         """
         return re.sub("(^<P>|</P>$)", "", non_p_string, flags=re.IGNORECASE)
 
     def cycling(value: str) -> str:
-        div_start = "<span class=\"cycling-words inline-block text-left whitespace-nowrap\">"
+        div_start = (
+            '<span class="cycling-words inline-block text-left whitespace-nowrap">'
+        )
         div_stop = "</span>"
-        span_start = "<span class=\"bg-gray-100 dark:bg-[#2A2D2E]\">"
+        span_start = '<span class="bg-gray-100 dark:bg-[#2A2D2E]">'
         span_stop = "</span>"
-        return (value
-            .replace("%(", f"{div_start}{span_start}")
+        return (
+            value.replace("%(", f"{div_start}{span_start}")
             .replace("|", f"{span_stop}{span_start}")
             .replace(")%", f"{span_stop}{div_stop}")
         )
@@ -46,32 +54,56 @@ def main() -> None:
     jinja_env.filters["no_p"] = no_p
     jinja_env.filters["cycling"] = cycling
     template = jinja_env.get_template("index.html")
+    template_donate = jinja_env.get_template("donate.html")
 
     # Load blog data
-    with open('blog.json', 'r') as file:
+    with open("blog.json", "r") as file:
         data = json.load(file)
-    articles = [{
-        "title": article.get("unicode_title", article["title"]),
-        "link": f"https://forum.yunohost.org/t/{article['slug']}/{article['id']}",
-        "date": article["created_at"]
-    } for article in data["topic_list"]["topics"] if article["visible"]]
-    articles.sort(key=lambda item: item['date'], reverse=True)
+    articles = [
+        {
+            "title": article.get("unicode_title", article["title"]),
+            "link": f"https://forum.yunohost.org/t/{article['slug']}/{article['id']}",
+            "date": article["created_at"],
+        }
+        for article in data["topic_list"]["topics"]
+        if article["visible"]
+    ]
+    articles.sort(key=lambda item: item["date"], reverse=True)
     articles = articles[0:6]
 
     # Fetch weblate statistics to list locales translated abode a certain treshold
-    j = requests.get("https://translate.yunohost.org/api/components/yunohost/landingpage/statistics/").json()
+    j = requests.get(
+        "https://translate.yunohost.org/api/components/yunohost/landingpage/statistics/"
+    ).json()
 
     def get_lang_native_name(code):
         return Language.make(language=code).display_name(code).title()
 
-    relevant_langs = {lang["code"]: get_lang_native_name(lang["code"]) for lang in j["results"] if lang["translated_percent"] > 50}
+    relevant_langs = {
+        lang["code"]: get_lang_native_name(lang["code"])
+        for lang in j["results"]
+        if lang["translated_percent"] > 50
+    }
     relevant_langs = dict(sorted(relevant_langs.items()))
 
     # Generate 'en'
     locale = "en"
-    translated_html = template.render({"lang": locale, "articles": articles, "langs": relevant_langs, "_": lambda s: s})
+    translated_html = template.render(
+        {
+            "lang": locale,
+            "articles": articles,
+            "langs": relevant_langs,
+            "_": lambda s: s,
+        }
+    )
     with open(f"{args.output}/index.{locale}.html", "w") as file:
         file.write(translated_html)
+
+    translated_donate_html = template_donate.render(
+        {"lang": locale, "langs": relevant_langs, "_": lambda s: s}
+    )
+    with open(f"{args.output}/donate.{locale}.html", "w") as file:
+        file.write(translated_donate_html)
 
     # Generate translated html for each locales
     locales = [localedir.name for localedir in LOCALES_PATH.iterdir()]
@@ -80,10 +112,19 @@ def main() -> None:
             continue
         translations = Translations.load(LOCALES_PATH, [locale])
         jinja_env.install_gettext_translations(translations)  # type: ignore
-        translated_html = template.render({"lang": locale, "articles": articles, "langs": relevant_langs})
+        translated_html = template.render(
+            {"lang": locale, "articles": articles, "langs": relevant_langs}
+        )
 
         with open(f"{args.output}/index.{locale}.html", "w") as file:
             file.write(translated_html)
+
+        translated_donate_html = template_donate.render(
+            {"lang": locale, "langs": relevant_langs}
+        )
+
+        with open(f"{args.output}/donate.{locale}.html", "w") as file:
+            file.write(translated_donate_html)
 
 
 if __name__ == "__main__":

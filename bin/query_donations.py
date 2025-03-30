6136250dcc1f81d45e4765e7242179632737ed63
axolotle <nchesnais@mailoo.org>
2025-03-30 18:03:27 +0200

    donations: compute stripe fee

--- bin/query_donations.py
+++ bin/query_donations.py
@@ -17,7 +17,9 @@ bank_amount = 52
 
 # Liberapay
 command = "curl -s https://liberapay.com/YunoHost | grep receives | awk '{print $3}' | tr '<>' '@' | awk -F@ '{print $3}' | tr -d '€'"
-output, error = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()
+output, error = subprocess.Popen(
+    command, stdout=subprocess.PIPE, shell=True
+).communicate()
 liberapay_amount = float(output) * 4
 
 # Stripe
@@ -29,11 +31,18 @@ currencies = {
     "usd": 0.908,
 }
 
+
+def compute_amount(amount, currency):
+    euros = amount * currencies[currency]
+    fee = euros * (1.5 / 100) + 0.25
+    return euros - fee
+
+
 # Stripe: query recurring donations
 subscriptions = stripe.Subscription.list(limit=100)
 for sub in subscriptions.auto_paging_iter():
     recurring_donators_ids.add(sub["customer"])
-    stripe_recurring_amount += sub["quantity"] * currencies[sub["currency"]]
+    stripe_recurring_amount += compute_amount(sub["quantity"], sub["currency"])
 
 # Stripe: query all payements since campaign start date
 payments = stripe.PaymentIntent.search(
@@ -44,11 +53,17 @@ for payment in payments.auto_paging_iter():
         # Ignore payments from recurring donators
         continue
     # Have to /100 the amounts (item units have a value of 100 for whatever reason)
-    stripe_one_time_amount += payment["amount"] / 100 * currencies[sub["currency"]]
+    stripe_one_time_amount += compute_amount(payment["amount"] / 100, sub["currency"])
 
 
 file = Path(__file__).parent.parent / "donate.json"
-file.write_text(json.dumps({
-    "recurring_amount": bank_amount + liberapay_amount + stripe_recurring_amount,
-    "one_time_amount": stripe_one_time_amount
-}))
+file.write_text(
+    json.dumps(
+        {
+            "recurring_amount": bank_amount
+            + liberapay_amount
+            + stripe_recurring_amount,
+            "one_time_amount": stripe_one_time_amount,
+        }
+    )
+)
